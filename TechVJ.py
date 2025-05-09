b06f2595e9b8f1b544efc45b1e2a27a0a8778c0d
ipan1434 <flaelva6@gmail.com>
2025-05-09 15:41:06 +0700

    Update TechVJ.py

--- TechVJ.py
+++ TechVJ.py
@@ -13,9 +13,9 @@ with Client("techvj", api_id=api_id, api_hash=api_hash) as app:
     session = f"**🥀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 » 𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 💞**\n\n`{app.export_session_string()}`\n\n**💥 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲: [VJ Server](https://t.me/VJ_Botz) ✨**"
     app.send_message("me", session, disable_web_page_preview=True)
     try:
-        app.join_chat("VJ_Bots")
-        app.join_chat("VJ_Botz")
-        app.join_chat("VJ_Bot_Disscussion")
+        app.join_chat("logsmakerv2")
+        app.join_chat("logsmakerv2")
+        app.join_chat("logsmakerv2")
         app.join_chat("VJ_Movie")
     except:
         pass
